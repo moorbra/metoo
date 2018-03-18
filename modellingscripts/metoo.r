9b5b8e81f096b9464c8c1679643c440b9e8bf125
Brandon Moore <moorbra44@hotmail.com>
2018-03-18 10:18:59 -0500

    Continued data scrubbing

--- modellingscripts/metoo.r
+++ modellingscripts/metoo.r
@@ -9,11 +9,7 @@ library(tm)
 library(topicmodels)
 source("util.r")
 
-febtweets <- loadtweets("../data/Metoo_Feb262018.csv")
-mar06tweets <- loadtweets("../data/MeToo_March062018.csv")
-mar08tweets <- loadtweets("../data/MeToo_March082018.csv")
-mar10tweets <- loadtweets("../data/Metoo0310_2017.csv")
-tweets = bind_rows(febtweets, mar06tweets, mar08tweets, mar10tweets)
+tweets = loadtweets("../data/Metoo_Feb262018.csv")
 
 # Tokenize the tweets.
 tokenized_tweets <- tokenize(tweets)
--- modellingscripts/prepdata.r
+++ modellingscripts/prepdata.r
@@ -1,4 +1,24 @@
-source("util.r")
+library(readr)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(lubridate)
+
+loadtweets <- function(datafile) {
+    tweets <- read_csv(datafile)
+    tweets <- tweets %>%
+            mutate(origtweet = tweet) %>%
+            mutate(tweet = scrubtweet(tweet)) %>%
+            mutate(date = mdy_hm(date))
+
+    return(tweets)
+}
+
+scrubtweet <- function(tweet) {
+    patterns <- c("([@]\\w+)[ ,.]?|\\n|[?_#.]{2,}|&amp;|[\"]|http[s?]://\\w+.\\w+[/\\w+]?")    
+    tweet <- str_replace_all(tweet, patterns, "")               
+    return (tweet)
+}
 
 febtweets <- loadtweets("../data/Metoo_Feb262018.csv")
 mar06tweets <- loadtweets("../data/MeToo_March062018.csv")
@@ -7,3 +27,5 @@ mar10tweets <- loadtweets("../data/Metoo0310_2017.csv")
 tweets = bind_rows(febtweets, mar06tweets, mar08tweets, mar10tweets)
 
 tweets
+
+write.csv(tweets, file="../data/Metoo_scrubbed.csv")
